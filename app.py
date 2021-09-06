4d75679ef4738c098456b1cbad84235e2e3317d8
Ignotusm <Vlkolenskymark@gmail.com>
2021-09-06 20:57:47 +0200

    Set formulaRaceWeek name as official name

--- app.py
+++ app.py
@@ -5,12 +5,15 @@ from bs4 import BeautifulSoup
 
 # class for f1 raceweek
 class formulaRaceWeek:
-    def __init__(self, name, events):
+    def __init__(self, name):
         self.name = name
-        self.events = events
+        self.events = []
+
+    def addEvent(self, event):
+        self.events.append(event)
 
     def printRaceWeek(self):
-        print("{}".format(self.name))
+        print(str(self.name))
         for event in self.events:
             event.printEvent()
 
@@ -32,39 +35,37 @@ class formulaEvent:
 def getTimeLine(link):
     # if there is no official schedule then:
     if("TBC" in link):
-        return print("No Race")
+        return
 
+    page = requests.get("https://www.formula1.com{}".format(link))
+    soup = BeautifulSoup(page.content, "html.parser")
+    location = soup.find("h2", class_="f1--s")
+    raceName = location.text
+    race = formulaRaceWeek(raceName)
     URL = "https://www.formula1.com{}/Timetable.html".format(
         link.replace(".html", ""))
     page = requests.get(URL)
+
     if page.status_code != 200:
-        print()
         page = requests.get("https://www.formula1.com{}".format(link))
         soup = BeautifulSoup(page.content, "html.parser")
         h1 = soup.find("h1")
-        print(h1.text)
+        print(type(h1.text))
         print()
         print("No official data yet")
         print()
         return
-
     soup = BeautifulSoup(page.content, "html.parser")
-    location = soup.find("h1")
-    print(location.text.strip("\n"))
     f1 = soup.find_all("td")
 
     for i in range(0, len(f1)):
         if "thursday" in f1[i].text.lower():
-            print()
             date = f1[i].text.strip("\n")
         if "friday" in f1[i].text.lower():
-            print()
             date = f1[i].text.strip("\n")
         if "saturday" in f1[i].text.lower():
-            print()
             date = f1[i].text.strip("\n")
         if "sunday" in f1[i].text.lower():
-            print()
             date = f1[i].text.strip("\n")
 
         if "Formula 1" in f1[i].text:
@@ -72,17 +73,20 @@ def getTimeLine(link):
                 name = f1[i+1].text.strip("\n")
                 time = f1[i+2].text.strip("\n")
                 event = formulaEvent(date, name, time)
-                event.printEvent()
+                race.addEvent(event)
             if "Qualifying" in f1[(i+1)].text:
                 name = f1[i+1].text.strip("\n")
                 time = f1[i+2].text.strip("\n")
                 event = formulaEvent(date, name, time)
-                event.printEvent()
+                race.addEvent(event)
             if "Grand Prix" in f1[(i+1)].text:
                 name = f1[i+1].text.strip("\n")
                 time = f1[i+2].text.strip("\n")
                 event = formulaEvent(date, name, time)
-                event.printEvent()
+                race.addEvent(event)
+
+    race.printRaceWeek()
+    print()
 
 
 URL = "https://www.formula1.com/en/racing/2021.html"
